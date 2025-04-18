c28c9a2c8ddd1894732f8fb1af7c08b72452b6e2
Moses Oghene <mosesogheno@gmail.com>
2025-04-18 04:50:17 +0100

    made migrations

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "alllauth",
+    "allauth",
     "allauth.account",
     "allauth.socialaccount",
 
--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
 
