04f8fbf56b11a55362bc0ae1a32504ae19810a03
Moses Oghene <mosesogheno@gmail.com>
2025-04-18 06:11:47 +0100

    SMTP Email config

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,14 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mosesogheneo@gmail.com'
+EMAIL_HOST_PASSWORD = 'nnudrbzwgkhjdzab'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
