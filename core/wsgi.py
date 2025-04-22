6fa39d773579c0cee44852fd604343a6f03cd73e
Moses Oghene <mosesogheno@gmail.com>
2025-04-22 12:02:52 +0100

    APP Config

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
