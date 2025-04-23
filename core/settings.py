0c8a6de81c1fbb8510adef4d05353cef834972a9
Moses Oghene <mosesogheno@gmail.com>
2025-04-23 17:16:46 +0100

    Updated settings for deployment

--- core/settings.py
+++ core/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://licitex.onrender.com"]
+ALLOWED_HOSTS = ['https://licitex.onrender.com']
 
 
 # Application definition
