ad9351082d6fe5069fcff52d296cb8f35ba79d08
Moses Oghene <mosesogheno@gmail.com>
2025-04-23 17:19:45 +0100

    Updated settings for deployment

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://licitex.onrender.com']
+ALLOWED_HOSTS = ['licitex.onrender.com']
 
 
 # Application definition
